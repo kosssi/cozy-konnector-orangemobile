d88af8f1f898b3b51ea7997d73cf66a7839c7cd9
jacquarg <guillaume.jacquart@hoodbrains.com>
2017-05-23 15:45:44 +0200

    Start from orangevod basis.

---
+++ README.md
@@ -0,0 +1,128 @@
+[Cozy][cozy] Orange VOD konnector
+=================================
+
+What's Cozy?
+------------
+
+![Cozy Logo](https://cdn.rawgit.com/cozy/cozy-guidelines/master/templates/cozy_logo_small.svg)
+
+[Cozy] is a platform that brings all your web services in the same private space.  With it, your webapps and your devices can share data easily, providing you with a new experience. You can install Cozy on your own hardware where no one's tracking you.
+
+
+What's this new konnector?
+--------------------------
+
+This connector will download data from your Orange account on your Cozy. It will
+automatically download the list of movies you downloaded in free (TV Replay) or paid VOD from
+01/01/2015 (adult contents are not included). You will be able to use these data in different
+apps in your Cozy.
+
+### Open a Pull-Request
+
+If you want to work on this konnector and submit code modifications, feel free to open pull-requests! See the [contributing guide][contribute] for more information about how to properly open pull-requests.
+
+### Run
+
+If you have a running accessible cozy-stack you can test your modifications to the konnector without installing
+and/or updating the konnector in the cozy-stack :
+
+You first need an installed [nodejs] (LTS version is fine).
+
+Then just run (but you need to have proper COZY_CREDENTIALS, COZY_URL and COZY_FIELDS environment variables):
+
+```sh
+npm install --global yarn
+```
+
+Then run (but you have to have proper COZY_CREDENTIALS, COZY_URL and COZY_FIELDS environment variables):
+
+```sh
+yarn
+yarn start
+```
+
+Where:
+ - COZY_CREDENTIALS needs to be the result of ```cozy-stack instances token-cli <instance name> <scope>```
+ - COZY_URL is the full http url to your cozy
+ - COZY_FIELDS is something like :
+```javascript
+{
+  "data":{
+    "attributes":{
+      "arguments":{
+        "account":"cf31eaef5d899404a7e8c3737c1c2d1f",
+        "folder_to_save":"folderPath",
+        "slug":"mykonnector"
+      }
+    }
+  }
+}
+```
+
+The "account" field is the id of the record with doctype "io.cozy.accounts" which will be used as
+parameters for your konnector.
+
+### Test
+
+If you do not want to have to install the konnector on a cozy v3 to test it, you can register the
+konnector as an OAuth application with the following commands :
+
+```sh
+yarn
+yarn dev
+```
+
+This command will register your konnector as an OAuth application to the cozy-stack. By default,
+the cozy-stack is supposed to be located in http://cozy.tools:8080. If this is not your case, just
+update the COZY_URL field in [./data/env.js].
+
+After that, your konnector is running but should not work since you did not specify any credentials to
+the target service. You can do this in a [./data/env_fields.json] (you have
+[./data/env_fields.json.template] available as a template)
+
+Now run ```yarn init:dev:account``` to create an account in the targeted cozy which will be used by
+the connector (the id of the account is saved in ./data/account.txt)
+
+Now run `yarn dev` one more time, it should be ok.
+
+### Hack
+
+If you do not want to need to have an accessible cozy-stack, just run :
+
+```sh
+yarn
+yarn standalone
+```
+
+The requests to the cozy-stack will be stubbed using the [./data/fixture.json] file as source of data
+and when cozy-client is asked to create or update data, the data will be outputed to the console.
+The bills (or any file) will be saved in the ./data directory.
+
+### Maintainer
+
+The lead maintainers for this konnector is <TODO>
+
+
+### Get in touch
+
+You can reach the Cozy Community by:
+
+- Chatting with us on IRC [#cozycloud on Freenode][freenode]
+- Posting on our [Forum]
+- Posting issues on the [Github repos][github]
+- Say Hi! on [Twitter]
+
+
+License
+-------
+
+Orange VOD konnector is developed by cozy and distributed under the [AGPL v3 license][agpl-3.0].
+
+[cozy]: https://cozy.io "Cozy Cloud"
+[agpl-3.0]: https://www.gnu.org/licenses/agpl-3.0.html
+[freenode]: http://webchat.freenode.net/?randomnick=1&channels=%23cozycloud&uio=d4
+[forum]: https://forum.cozy.io/
+[github]: https://github.com/cozy/
+[nodejs]: https://nodejs.org/
+[twitter]: https://twitter.com/mycozycloud
+[yarn]: https://yarnpkg.com
---
+++ konnector.js
@@ -0,0 +1,188 @@
+'use strict'
+
+const request = require('request')
+// require('request-debug')(request)
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+
+// const localization = require('../lib/localization_manager')
+const {log, baseKonnector, updateOrCreate, models} = require('cozy-konnector-libs')
+
+const VideoStream = models.baseModel.createNew({name: 'io.cozy.videostream', displayName: 'videostream'})
+
+const API_ROOT = 'https://mesinfos.orange.fr'
+
+/*
+ * The goal of this connector is to fetch event from facebook and store them
+ * in the Cozy
+ */
+const connector = module.exports = baseKonnector.createNew({
+  name: 'Orange VOD',
+  customView: '<%t konnector customview orange_vod %>',
+
+  connectUrl: 'https://mesinfos.orange.fr/auth?redirect_url=',
+  category: 'isp',
+  color: {
+    hex: '#FF6600',
+    css: '#FF6600'
+  },
+
+  fields: {
+    frequency: {
+      type: 'dropdown',
+      default: 'weekly',
+      advanced: true,
+      options: ['hourly', 'daily', 'weekly', 'monthly']
+    },
+    access_token: {
+      type: 'hidden'
+    },
+    lastVideoStream: {
+      type: 'hidden'
+    }
+  },
+  dataType: ['videostream'],
+  models: [VideoStream],
+
+  fetchOperations: [
+    checkToken,
+    downloadVod,
+    updateOrCreate(null, VideoStream, ['clientId', 'timestamp'])
+    // saveFieldsInKonnector,
+    // buildNotifContent
+  ]
+
+})
+
+function checkToken (requiredFields, entries, data, next) {
+  log('info', 'requiredFields')
+  log('info', requiredFields)
+  const token = requiredFields.access_token
+  if (!token) { return next('token not found') }
+
+  try {
+    // let payload = token.split('.')[1]
+    // payload = JSON.parse(new Buffer(payload, 'base64').toString())
+
+    // if (payload.token_type !== 'fixe') {
+    //   connector.logger.error(`Wronk token_type for this konnector: ${payload.token_type}`)
+    //   return next('not fixe token')
+    // }
+
+    next()
+  } catch (e) {
+    log('error', `Unexpected token format: ${e}`)
+    next('token not found')
+  }
+}
+
+function requestOrange (uri, token, callback) {
+  log('info', uri)
+
+  request.get(uri, { auth: { bearer: token }, json: true }, (err, res, body) => {
+    if (err) {
+      log('error', `Download failed: ${err}`)
+      return callback(err)
+    }
+    if (res.statusCode.toString() !== '200') {
+      err = `${res.statusCode} - ${res.statusMessage} ${err || ''}`
+      log('error', body)
+    }
+
+    callback(null, body)
+  })
+}
+
+function downloadVod (requiredFields, entries, data, next) {
+  log('info', 'Downloading vod data from Orange...')
+  let uri = `${API_ROOT}/data/vod`
+  if (requiredFields.lastVideoStream) {
+    uri += `?start=${requiredFields.lastVideoStream.slice(0, 19)}`
+  }
+
+  requestOrange(uri, requiredFields.access_token, (err, body) => {
+    if (err) { return next(err) }
+    entries.videostreams = []
+    if (body.forEach) body.forEach((vod) => {
+      if (vod.ts && requiredFields.lastVideoStream < vod.ts) {
+        requiredFields.lastVideoStream = vod.ts
+      }
+      if (vod.err) { return }
+
+      entries.videostreams.push({
+        docType: 'VideoStream',
+        docTypeVersion: connector.docTypeypeVersion,
+        content: {
+          type: vod.cont_type,
+          title: vod.cont_title,
+          subTitle: vod.cont_subtitle,
+          duration: vod.cont_duration,
+          quality: vod.cont_format,
+          publicationYear: vod.prod_dt,
+          country: vod.prod_nat,
+          id: vod.cont_id,
+          longId: vod.src_id,
+          adultLevel: vod.adult_level === 'none' ? undefined : vod.adult_level,
+          csaCode: vod.csa_code
+        },
+        price: vod.price,
+        timestamp: vod.ts,
+        viewingDuration: vod.use_duration ? Math.round(Number(vod.use_duration) * 60) : undefined,
+        details: {
+          offer: vod.offer,
+          offerName: vod.offer_name,
+          service: vod.service,
+          network: vod.net,
+          techno: vod.techno,
+          device: vod.device,
+          platform: vod.platf
+        },
+        action: vod.action,  // visualisation or command
+        clientId: vod.line_id
+      })
+    })
+
+    next()
+  })
+}
+
+// // Save konnector's fieldValues during fetch process.
+// function saveFieldsInKonnector (requiredFields, entries, data, next) {
+//   connector.logger.info('saveFieldsInKonnector')
+
+//   // Disable eslint because we can't require models/konnector at the top
+//   // of this file (or Konnector will be empty). It's because in the require
+//   // tree of models/konnector, there is the current file.
+//   //eslint-disable-next-line
+//   const Konnector = require('../models/konnector');
+
+//   Konnector.get(connector.slug, (err, konnector) => {
+//     if (err) {
+//       connector.logger.error(err)
+//       return next('internal error')
+//     }
+
+//     const accounts = konnector.accounts
+//     const index = accounts.findIndex(account =>
+//         account.access_token === requiredFields.access_token)
+//     accounts[index] = requiredFields
+//     konnector.updateFieldValues({ accounts }, next)
+//   })
+// }
+
+// function buildNotifContent (requiredFields, entries, data, next) {
+//   // data.updated: we don't sepak about update, beacause we don't now if the
+//   // update actually changes the data or not.
+
+//   // Signal all add of document.
+//   const addedList = []
+//   Object.keys(data.created).forEach((docsName) => {
+//     const count = data.created[docsName]
+//     if (count > 0) {
+//       addedList.push(localization.t(
+//         `notification ${docsName}`, { smart_count: count }))
+//     }
+//   })
+
+//   entries.notifContent = addedList.join(', ')
+//   next()
+// }
