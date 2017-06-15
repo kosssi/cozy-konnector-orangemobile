c5e6ae87482d267f3107bbbaa5791bb40eedb765
doubleface <christophe@thiriot.xyz>
2017-06-15 16:56:25 +0200

    fix: removed unsed folderPath in index.js which caused NOT_EXISTING_DIRECTORY error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {log} = require('cozy-konnector-libs')
 
 const cozyFields = JSON.parse(process.env.COZY_FIELDS)
 
-konnector.fetch({account: cozyFields.account, folderPath: cozyFields.folder_to_save}, err => {
+konnector.fetch({account: cozyFields.account}, err => {
   log('debug', 'The konnector has been run')
   if (err) {
     log('error', err)
